c6240cb38a5a0c47691e0bf06d8d9b5bd6da4033
Thai Hung TRAN <htran1@mantu.com>
2021-11-05 15:09:55 +0700

    Add project files.

---
+++ GroupDocs.Viewer.Net5/Program.cs
@@ -0,0 +1,29 @@
+﻿using GroupDocs.Viewer.Options;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace GroupDocs.Viewer.Net5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GroupDocsViewer Test on .Net 5");
+            try
+            {
+                var buildDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                var filePath = buildDir + @"\Test.pdf";
+                Stream fs = File.OpenRead(filePath);
+                using Viewer viewer = new Viewer(fs);
+                HtmlViewOptions viewOptions = HtmlViewOptions.ForEmbeddedResources();
+                viewOptions.RenderToSinglePage = true;
+                viewer.View(viewOptions);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ GroupDocs.Viewer.Net6/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GroupDocs.Viewer;
+using GroupDocs.Viewer.Options;
+using System.Reflection;
+
+Console.WriteLine("GroupDocsViewer Test on .Net 6");
+try
+{
+    var buildDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    var filePath = buildDir + @"\Test.pdf";
+    Stream fs = File.OpenRead(filePath);
+    using Viewer viewer = new Viewer(fs);
+    HtmlViewOptions viewOptions = HtmlViewOptions.ForEmbeddedResources();
+    viewOptions.RenderToSinglePage = true;
+    viewer.View(viewOptions);
+}
+catch (Exception ex)
+{
+    throw ex;
+}
